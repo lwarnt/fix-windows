4ab939a79ff0dc9291deacb0b651e9046dc9647a
Leroy <leroy.warnt@flane.de>
2022-08-03 11:57:07 +0200

    fix: Clarify usage on wrong input

--- install-windows11/make-iso.sh
+++ install-windows11/make-iso.sh
@@ -2,12 +2,15 @@
 NOCOLOR='\e[0m'
 YELLOW='\e[33m'
 RED='\e[31m'
+INFO="[INFO]: "
+ERROR="[ERROR]: "
 
 # crude input validation
 if [[ "$1" =~ ^/ ]] && [[ "$2" =~ ^/ ]]; then
     :
 else 
-    printf "${RED}Paths must be absolute.\n${NOCOLOR}"
+    printf "Usage: ./make-iso.sh /path/to/Windows10.iso /path/to/Windows11.iso \n"
+    printf "${RED}${ERROR}Paths must be absolute.\n${NOCOLOR}"
     exit 1
 fi
 
@@ -15,23 +18,23 @@ W10_MOUNT="/mnt/win10"
 W10_WORKDIR="/tmp/win10-unpacked"
 W11_MOUNT="/mnt/win11"
 
-printf "${YELLOW}Create directories.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Create directories.\n${NOCOLOR}"
 DIRS=( "$W10_MOUNT" "$W10_WORKDIR" "$W11_MOUNT" )
 for dir in ${DIRS[@]}; do
     mkdir -p $dir
 done
 
-printf "${YELLOW}Mount disk images.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Mount disk images.\n${NOCOLOR}"
 mount -o loop "$1" "$W10_MOUNT"
 mount -o loop "$2" "$W11_MOUNT"
 
-printf "${YELLOW}Copy files to working directory.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Copy files to working directory.\n${NOCOLOR}"
 cp -R "$W10_MOUNT"/* "$W10_WORKDIR"
 
-printf "${YELLOW}Copy Windows image file to working directory.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Copy Windows image file to working directory.\n${NOCOLOR}"
 cp "$W11_MOUNT"/sources/install.wim "$W10_WORKDIR"/sources/
 
-printf "${YELLOW}Package the new disk image.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Package the new disk image.\n${NOCOLOR}"
 # Source: https://unix.stackexchange.com/questions/531012/how-to-modify-an-installation-iso-and-keep-it-bootable
 # -b needs to be relative -.-
 cd "$W10_WORKDIR"
@@ -49,7 +52,7 @@ genisoimage --allow-limited-size \
     -o ../Windows11-out.iso \
     .
 
-printf "${YELLOW}Cleanup.\n${NOCOLOR}"
+printf "${YELLOW}${INFO}Cleanup.\n${NOCOLOR}"
 umount "$W10_MOUNT"
 umount "$W11_MOUNT"
 rm -rf "$W10_MOUNT"
